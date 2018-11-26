bf97a0fbacfd1a1126005e56a31993bb0a608c9d
Augusto <augustoamaro@icloud.com>
2018-11-26 12:10:58 -0200

    db ajustes

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-// Define database
-define('dbhost', 'localhost');
-define('dbuser', 'root');
-define('dbpass', '');
-define('dbname', 'petclean');
 
-// Connecting database
+define('dbhost', 'us-cdbr-iron-east-01.cleardb.net');
+define('dbuser', 'bc82c92e70e9fd');
+define('dbpass', '33c4ec08');
+define('dbname', 'heroku_74944fad105d65a');
+
+
 try {
 	$connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
 	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
